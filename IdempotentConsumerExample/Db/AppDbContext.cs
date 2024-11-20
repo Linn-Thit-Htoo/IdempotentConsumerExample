322b9172a82f554a1cf2d8a46aa3746d46636ded
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-20 23:58:43 +0630

    -

--- IdempotentConsumerExample/Db/AppDbContext.cs
+++ IdempotentConsumerExample/Db/AppDbContext.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace IdempotentConsumerExample.Db
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
-        }
+namespace IdempotentConsumerExample.Db;
 
-        public DbSet<Tbl_Messages> Tbl_Messages { get; set; }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
     }
+
+    public DbSet<Tbl_Messages> Tbl_Messages { get; set; }
 }
