7ba66c63a15e0485f429164411f68f8c2d7a7fb4
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-20 23:59:09 +0630

    -

--- IdempotentConsumerExample/Models/BlogRequestModel.cs
+++ IdempotentConsumerExample/Models/BlogRequestModel.cs
@@ -1,11 +1,10 @@
-﻿namespace IdempotentConsumerExample.Models
+﻿namespace IdempotentConsumerExample.Models;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
-        public string MessageId { get; set; }
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public string MessageId { get; set; }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
