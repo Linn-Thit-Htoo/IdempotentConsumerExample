067d15146649a2fcdce3962fe691f062a83e015c
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-20 23:59:53 +0630

    -

--- IdempotentConsumerExample/Program.cs
+++ IdempotentConsumerExample/Program.cs
@@ -14,11 +14,8 @@ builder
     )
     .AddEnvironmentVariables();
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -32,7 +29,6 @@ builder.Services.AddHostedService<RabbitMQService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
